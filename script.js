90e770f749f109487d5d8b7b6fbf9d61dfdd4cad
bbrintle <bbrintle@gmail.com>
2020-10-05 10:55:34 -0700

    checking view of readme

---
+++ script.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+async function getUserInfo(){
+    try {
+    }
+    catch(err){
+    console.log(err);
+};
+};
