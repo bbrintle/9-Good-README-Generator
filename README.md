8d25cdfec9b979afe73b3933f1701bd337630e23
bbrintle <bbrintle@gmail.com>
2020-10-05 11:47:47 -0700

    testing readability of README

--- README.md
+++ README.md
@@ -1,39 +1,40 @@
-# {Title}
 
-## Table of Contents
-1. [ Description ](#desc)
-2. [ Insallation ](#install)
-3. [ Usage ](#usage)
-4. [ License ](#lic)
-5. [ Contributing ](#contrib)
-6. [ Tests ](#test)
-7. [ Questions ](#quest)
+    # ButtLick
 
-<a name="desc"></a>
-## 1. Description
-{userInput}
-
-<a name="install"></a>
-## 2. Installation
-{userInput}
-
-<a name="usage"></a>
-## 3. Usage
-{userInput}
-
-<a name="lic"></a>
-## 4. License
-{userSelectionFromList}
-
-<a name="contrib"></a>
-## 5. Contributing
-{userInput}
-
-<a name="test"></a>
-## 6. Tests
-{userInput}
-
-<a name="quest"></a>
-## 7. Questions
-{link to users GitHub profile}
-{user's email}
+    ## Table of Contents
+    1. [ Description ](#desc)
+    2. [ Insallation ](#install)
+    3. [ Usage ](#usage)
+    4. [ License ](#lic)
+    5. [ Contributing ](#contrib)
+    6. [ Tests ](#test)
+    7. [ Questions ](#quest)
+    
+    <a name="desc"></a>
+    ## 1. Description
+    DoodOO
+    
+    <a name="install"></a>
+    ## 2. Installation
+    Just do it
+    
+    <a name="usage"></a>
+    ## 3. Usage
+    cant use this
+    
+    <a name="lic"></a>
+    ## 4. License
+    MIT
+    
+    <a name="contrib"></a>
+    ## 5. Contributing
+    contibute all u can
+    
+    <a name="test"></a>
+    ## 6. Tests
+    testing 123
+    
+    <a name="quest"></a>
+    ## 7. Questions
+    https://www.github.com/bbrintle
+    Contact me at: bbrintle@gmail.com
--- script.js
+++ script.js
@@ -4,10 +4,114 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-async function getUserInfo(){
-    try {
-    }
-    catch(err){
-    console.log(err);
+function getUserInfo(){
+    return inquirer.prompt([
+        {
+        type: "input",
+        message: "What is the title of your prject?",
+        name: "title" 
+        },
+        {
+        type: "input",
+        message: "Please enter the description of your project:",
+        name: "desc" 
+        },
+        {
+        type: "input",
+        message: "Please enter the installation instructions:",
+        name: "installInfo" 
+        },
+        {
+        type: "input",
+        message: "Please enter the Usage information:",
+        name: "usageInfo" 
+        },
+        {
+        type: "input",
+        message: "Please enter the contribution guidelines for the project:",
+        name: "contrib" 
+        },
+        {
+        type: "input",
+        message: "Please enter the test instructions:",
+        name: "testInfo" 
+        },
+        {
+        type: "list",
+        message: "Please select a license:",
+        name: "license",
+        choices: [
+            "GPLv3",
+            "GPLv2",
+            "BSD",
+            "MIT",
+            "Apache License 2.0"
+        ]
+        },
+        {
+        type: "input",
+        message: "Please enter your GitHub username:",
+        name: "gitHubUserName" 
+        },
+        {
+        type: "input",
+        message: "Please enter your email:",
+        name: "userEmail" 
+        }
+    ]);
 };
-};
+
+function generateREADMEText(answers){
+    return `
+    # ${answers.title}
+
+    ## Table of Contents
+    1. [ Description ](#desc)
+    2. [ Insallation ](#install)
+    3. [ Usage ](#usage)
+    4. [ License ](#lic)
+    5. [ Contributing ](#contrib)
+    6. [ Tests ](#test)
+    7. [ Questions ](#quest)
+    
+    <a name="desc"></a>
+    ## 1. Description
+    ${answers.desc}
+    
+    <a name="install"></a>
+    ## 2. Installation
+    ${answers.installInfo}
+    
+    <a name="usage"></a>
+    ## 3. Usage
+    ${answers.usageInfo}
+    
+    <a name="lic"></a>
+    ## 4. License
+    ${answers.license}
+    
+    <a name="contrib"></a>
+    ## 5. Contributing
+    ${answers.contrib}
+    
+    <a name="test"></a>
+    ## 6. Tests
+    ${answers.testInfo}
+    
+    <a name="quest"></a>
+    ## 7. Questions
+    https://www.github.com/${answers.gitHubUserName}
+    Contact me at: ${answers.userEmail}`;
+}
+    
+getUserInfo()
+    .then(function(answers){
+        const text = generateREADMEText(answers);
+        return writeFileAsync("README.md", text);
+    })
+    .then(function(){
+        console.log("Successfully created README file")
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
